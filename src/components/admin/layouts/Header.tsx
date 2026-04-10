b5a4b712e35b36e7210f63110d7cd5b16c0c2fd5
BhupendraBalapure <bhupendrabalapure@gmail.com>
2026-04-10 13:01:24 +0530

    hii

---
+++ src/components/admin/layouts/Header.tsx
@@ -0,0 +1,8 @@
+function Header(){
+    return (
+        <header className="bg-gray-800 text-white p-4">
+            <h1 className="text-xl font-bold">header</h1>
+        </header>
+    )
+}
+export default Header;
